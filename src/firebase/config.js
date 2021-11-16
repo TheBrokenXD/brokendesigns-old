ae3d0aa7fea1c9f9110bae85cbf08dac99ab50c6
Broken <broken.contact.1211@gmail.com>
2021-11-17 00:04:18 +0530

    hosting

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+// firebase config
+const firebaseConfig = {
+  apiKey: "AIzaSyBJkIwbHULQBg_nU7lsfIZx3w1-h48Q-vo",
+  authDomain: "brokendesigns-11bdd.firebaseapp.com",
+  projectId: "brokendesigns-11bdd",
+  storageBucket: "brokendesigns-11bdd.appspot.com",
+  messagingSenderId: "76018226693",
+  appId: "1:76018226693:web:60d03a08c7d1e396dcee6c"
+};
+
+// initialize
+const app = initializeApp(firebaseConfig);
